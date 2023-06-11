6b3e933b8ae3b8db64fee769b09abdf80381cc8b
Miguel Ferreira Cordero <mferreiracordero@gmail.com>
2023-06-11 18:04:31 +0200

    Implemented sign out and change password
    Took 53 minutes

--- app/src/main/java/com/mfc/celiacare/ui/account/AccountFragment.java
+++ app/src/main/java/com/mfc/celiacare/ui/account/AccountFragment.java
@@ -1,5 +1,7 @@
 package com.mfc.celiacare.ui.account;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,15 +14,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.mfc.celiacare.R;
 
 public class AccountFragment extends Fragment {
@@ -30,10 +36,9 @@ public class AccountFragment extends Fragment {
     FirebaseDatabase fDatabase;
     DatabaseReference databaseReference;
 
-    ImageView avatarIv;
     TextView nameTv;
     TextView emailTv;
-    Button btnExit;
+    Button signOutButton, changePasswordButton;
 
     public AccountFragment() {
 
@@ -66,18 +71,33 @@ public class AccountFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        avatarIv = view.findViewById(R.id.avatarIv);
         nameTv = view.findViewById(R.id.nameTv);
         emailTv = view.findViewById(R.id.emailTv);
-        btnExit = view.findViewById(R.id.btnExit);
-        btnExit.setOnClickListener(new View.OnClickListener() {
+        getUserData();
+        signOutButton = view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logout();
             }
         });
-        //Query query = databaseReference.child("email").equalTo(fUser.getEmail());
-        //query.addChildEventListener(new View());
+        changePasswordButton = view.findViewById(R.id.change_password_button);
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePassword();
+            }
+        });
+    }
+
+    private void getUserData() {
+        if (fUser != null) {
+            String name = fUser.getDisplayName();
+            String email = fUser.getEmail();
+
+            nameTv.setText(name);
+            emailTv.setText(email);
+        }
     }
 
     @Override
@@ -92,6 +112,103 @@ public class AccountFragment extends Fragment {
             navController.navigate(R.id.action_navigation_account_to_navigation_login);
         }
     }
+
+    private void changePassword() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle(getString(R.string.change_password));
+
+        LinearLayout layout = new LinearLayout(requireContext());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(50, 0, 50, 0);
+
+        final EditText currentPasswordEditText = new EditText(requireContext());
+        currentPasswordEditText.setHint(getString(R.string.current_password));
+        layout.addView(currentPasswordEditText);
+
+        final EditText newPasswordEditText = new EditText(requireContext());
+        newPasswordEditText.setHint(getString(R.string.new_password));
+        layout.addView(newPasswordEditText);
+
+        final EditText newPassword2EditText = new EditText(requireContext());
+        newPassword2EditText.setHint(getString(R.string.repite_contra));
+        layout.addView(newPassword2EditText);
+
+        builder.setView(layout);
+
+        builder.setPositiveButton(getString(R.string.change), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String currentPassword = currentPasswordEditText.getText().toString().trim();
+                String newPassword = newPasswordEditText.getText().toString().trim();
+                String newPassword2 = newPassword2EditText.getText().toString().trim();
+
+                String action = checkPassword(currentPassword, newPassword, newPassword2);
+                if (action.isEmpty()) {
+                    // Cambiar la contraseña del usuario
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user != null) {
+                        AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+                        user.reauthenticate(credential)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        user.updatePassword(newPassword)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Toast.makeText(requireContext(), getString(R.string.password_changed), Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(requireContext(), "Error al cambiar la contraseña.", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(requireContext(), "Asegúrese de que su contraseña actual esté bien", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                } else {
+                    Toast.makeText(requireContext(), action, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private String checkPassword(String currentPassword, String newPassword, String newPassword2) {
+        if (currentPassword.isEmpty()) {
+            return "Introduzca su contraseña actual.";
+        }
+        if (newPassword.isEmpty()) {
+            return "Introduzca su nueva contraseña.";
+        }
+        if (newPassword2.isEmpty()) {
+            return "Vuelva a introducir su nueva contraseña.";
+        }
+        if (newPassword.length() < 8) {
+            return "Su contraseña nueva tiene que tener más de 8 caracteres.";
+        }
+        if (!newPassword.equals(newPassword2)) {
+            return "Las nuevas contraseñas no coinciden.";
+        }
+        return ""; // No hay errores
+    }
+
 
     private void logout() {
         fAuth.signOut();
